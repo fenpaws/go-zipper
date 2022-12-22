36b3cc2b5b92df8f0517ad75aad3709317556060
SoXX <soxx@fenpa.ws>
2022-12-22 15:45:45 +0100

    refactor: :recycle: refactor

--- main.go
+++ main.go
@@ -2,32 +2,25 @@ package main
 
 import (
 	"fmt"
-	"github.com/caarlos0/env/v6"
-	"github.com/fenpaws/go-zipper/modules"
+	"github.com/fenpaws/go-zipper/modules/commands"
+	"github.com/fenpaws/go-zipper/modules/config"
+	"github.com/fenpaws/go-zipper/modules/helper"
+	"github.com/fenpaws/go-zipper/modules/telegramfiles"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 )
 
-type ConfigDatabase struct {
-	BotToken          string `env:"BOT_TOKEN"`
-	TelegramAPIServer string `env:"TELEGRAM_API_SERVER" envDefault:"https://api.telegram.org"`
-	Debug             bool   `env:"DEBUG" envDefault:"false"`
-}
-
 func main() {
-	// Parse environment variables
-	var cfg ConfigDatabase
-	if err := env.Parse(&cfg); err != nil {
-		log.Fatalf("Error parsing environment variables: %v", err)
-	}
+
+	Cfg, err := config.New()
 
 	// Initialize Telegram bot API
-	bot, err := tgbotapi.NewBotAPI(cfg.BotToken)
+	bot, err := tgbotapi.NewBotAPI(Cfg.BotToken)
 	if err != nil {
 		log.Fatalf("Error creating Telegram bot API: %v", err)
 	}
-	bot.SetAPIEndpoint(cfg.TelegramAPIServer + "/bot%s/%s")
-	bot.Debug = cfg.Debug
+	bot.SetAPIEndpoint(Cfg.TelegramAPIServer + "/bot%s/%s")
+	bot.Debug = Cfg.Debug
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	// Set update configuration
@@ -43,23 +36,23 @@ func main() {
 		// Process update if it contains a message
 		if update.Message != nil {
 			// Add document to files map if present in update message
-			if err := modules.AddFileToFiles(bot, update, files); err != nil {
+			if err := telegramfiles.AddFileToFiles(bot, update, files); err != nil {
 				log.Printf("Error adding file to files map: %v", err)
-				modules.ErrorFileToBig(err, bot, update)
+				helper.ErrorFileToBig(err, bot, update)
 
 			}
 
 			// Add photo to files map if present in update message
-			if err := modules.AddPhotoToFiles(bot, update, files); err != nil {
+			if err := telegramfiles.AddPhotoToFiles(bot, update, files); err != nil {
 				log.Printf("Error adding photo to files map: %v", err)
-				modules.ErrorFileToBig(err, bot, update)
+				helper.ErrorFileToBig(err, bot, update)
 			}
 
 			// Handle command if present in update message
-			modules.Command(bot, update.Message, files)
+			commands.Command(bot, update.Message, files)
 
 			// Print full JSON message if debugging is enabled
-			if cfg.Debug {
+			if Cfg.Debug {
 				if !update.Message.IsCommand() {
 					log.Printf(update.Message.Text)
 					for key, value := range files {
