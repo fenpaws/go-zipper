086ab5a3fb2ea1b756aef769134724888bdaa7c1
SoXX <soxx@fenpa.ws>
2023-01-05 11:33:26 +0100

    feat: require BOT_TOKEN

--- modules/config/config.go
+++ modules/config/config.go
@@ -3,7 +3,7 @@ package config
 import "github.com/caarlos0/env/v6"
 
 type ConfigDatabase struct {
-	BotToken          string `env:"BOT_TOKEN"`
+	BotToken          string `env:"BOT_TOKEN,required"`
 	TelegramAPIServer string `env:"TELEGRAM_API_SERVER" envDefault:"https://api.telegram.org"`
 	Debug             bool   `env:"DEBUG" envDefault:"false"`
 }
